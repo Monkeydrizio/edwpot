c07d0cf630052737fbae33377510850ba4c3536e
Administrator <Administrator@192.168.12.53>
2019-08-30 18:05:15 +0200

    lavoro dischi in vinile work in progress

--- src/s08/Checker.java
+++ src/s08/Checker.java
@@ -13,28 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/s08/checker")
 public class Checker extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String user = request.getParameter("user"); //(*)
-        Set<Character> set = new TreeSet<>();
-        if (user != null) {
-            for (char c : user.toCharArray()) {
-                set.add(Character.toLowerCase(c));
-            }
-        }
-        request.setAttribute("set", set); // attenzione, quello su cui lavora l'utente sono parametri(*) quelli su cui lavoro io sono attributi
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String user = request.getParameter("user"); // (*)
+		Set<Character> set = new TreeSet<>();
+		if (user != null) {
+			for (char c : user.toCharArray()) {
+				set.add(Character.toLowerCase(c));
+			}
+		}
+		request.setAttribute("set", set); // attenzione, quello su cui lavora l'utente sono parametri(*) quelli su cui
+											// lavoro io sono ATTRIBUTI. solo l'ultimo darà la response, quindi il
+											// lavoro già fatto dalla prima servlet vengno salvati nell'ATTRIBUTO della
+											// request e passati alla fase due.
 
-        RequestDispatcher rd = request.getRequestDispatcher("/s08/checker.jsp");
-        rd.forward(request, response);
-        
-    }
+		RequestDispatcher rd = request.getRequestDispatcher("/s08/checker.jsp"); //
+		rd.forward(request, response);
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
--- src/s09/Greeter.java
+++ src/s09/Greeter.java
@@ -15,44 +15,45 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/s09/greeter")
 public class Greeter extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        LocalTime start = (LocalTime) session.getAttribute("start");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession(); // non creo l'oggetto session xke lo crea il server Tomcat di suo
+		LocalTime start = (LocalTime) session.getAttribute("start"); // il downcast è pericoloso ma si usa fare qui cosi
+																		// senza instance of
 
-        Duration duration;
-        if (start == null) {
-            duration = Duration.ZERO;
-            session.setAttribute("start", LocalTime.now());
-        } else {
-            duration = Duration.between(start, LocalTime.now());
-        }
+		Duration duration;
+		if (start == null) {
+			duration = Duration.ZERO; // ZERO è la costante, una variabile final della classe duration
+			session.setAttribute("start", LocalTime.now());
+		} else {
+			duration = Duration.between(start, LocalTime.now());
+		}
 
-        if (request.getParameter("done") != null) {
-            session.invalidate();
+		if (request.getParameter("done") != null) {
+			session.invalidate();
 
-            response.setContentType("text/html");
-            response.setCharacterEncoding("utf-8");
-            try (PrintWriter writer = response.getWriter()) {
-                writer.println("<!DOCTYPE html><html>");
-                writer.println("<head><meta charset=\"utf-8\">");
-                writer.println("<title>So long</title></head>");
-                writer.println("<body><h1>Goodbye</h1>");
-                writer.println("<p>You worked on this stuff for " + duration.getSeconds() + " seconds</p>");
-                writer.println("</body></html>");
-            }
-            return;
-        }
+			response.setContentType("text/html");
+			response.setCharacterEncoding("utf-8");
+			try (PrintWriter writer = response.getWriter()) {
+				writer.println("<!DOCTYPE html><html>");
+				writer.println("<head><meta charset=\"utf-8\">");
+				writer.println("<title>So long</title></head>");
+				writer.println("<body><h1>Goodbye</h1>");
+				writer.println("<p>You worked on this stuff for " + duration.getSeconds() + " seconds</p>");
+				writer.println("</body></html>");
+			}
 
-        request.setAttribute("duration", duration);
-        RequestDispatcher rd = request.getRequestDispatcher("/s09/greeter.jsp");
-        rd.forward(request, response);
-    }
+		} else {
+			request.setAttribute("duration", duration);
+			RequestDispatcher rd = request.getRequestDispatcher("/s09/greeter.jsp");
+			rd.forward(request, response);
+		}
+	}
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
