088bc517202acfb680e567d8e98cdade60f4e895
Administrator <Administrator@192.168.12.53>
2019-09-02 10:13:25 +0200

    piccolo avanzamento esercizio vinili 02/09/19

--- src/alfy/AlfyPapRul.java
+++ src/alfy/AlfyPapRul.java
@@ -17,6 +17,21 @@ public class AlfyPapRul extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
+		if (session.getAttribute("v1") != null ||session.getAttribute("v2") != null || session.getAttribute("v3") != null || session.getAttribute("v4") != null || session.getAttribute("v5") != null) {
+		int numero_vinili_uno = (int) session.getAttribute("v1");
+		int numero_vinili_due = (int) session.getAttribute("v2");
+		int numero_vinili_tre = (int) session.getAttribute("v3");
+		int numero_vinili_quattro = (int) session.getAttribute("v4");
+		int numero_vinili_cinque = (int) session.getAttribute("v5");
+		int vinili_totali = numero_vinili_uno + numero_vinili_due + numero_vinili_tre + numero_vinili_quattro
+				+ numero_vinili_cinque;
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		try (PrintWriter writer = response.getWriter()) {
+			writer.print(vinili_totali);
+
+		}
+		}
 
 		if (request.getParameter("done") != null) {
 			session.invalidate();
@@ -25,7 +40,7 @@ public class AlfyPapRul extends HttpServlet {
 			try (PrintWriter writer = response.getWriter()) {
 				writer.println("<!DOCTYPE html><html>");
 				writer.println("<head><meta charset=\"utf-8\">");
-				writer.println("<title>So long</title></head>");
+				writer.println("<title>Purchase complete</title></head>");
 				writer.println("<body><h1>Goodbye</h1>");
 				writer.println("<p>CIAONE");
 				writer.println("</body></html>");
